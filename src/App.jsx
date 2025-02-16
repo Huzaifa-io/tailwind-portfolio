fbc6e95c470774d0a12b2f8eb12233df5a677bce
Muhammad-Huzaifa-786 <2008huzaifakhi@gmail.com>
2025-02-16 22:32:33 +0500

    tailwind-portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -102,6 +102,7 @@ export default function Portfolio() {
             "Express.js",
             "MongoDB",
             "Postman",
+            "Joi"
           ].map((skill) => (
             <div
               key={skill}
